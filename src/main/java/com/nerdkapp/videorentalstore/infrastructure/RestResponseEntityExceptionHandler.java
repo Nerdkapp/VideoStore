914454792a860e133452224e79f07f4fabb63744
Luciano Coccia <luciano.coccia@lastminute.com>
2016-10-05 21:41:33 +0200

    Added exception mapper for not found exceptions

---
+++ src/main/java/com/nerdkapp/videorentalstore/infrastructure/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.nerdkapp.videorentalstore.infrastructure;
+
+import com.nerdkapp.videorentalstore.domain.RentalNotFoundException;
+import com.nerdkapp.videorentalstore.domain.movies.MovieNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler
+{
+    @ExceptionHandler(value = {RentalNotFoundException.class, MovieNotFoundException.class})
+    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, null,
+          new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
